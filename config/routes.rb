0b2f7bbe38fc0c45ced1087eaed5398992f1843e
AlexRS90 <aramosbengy@gmail.com>
2022-04-10 01:18:11 -0500

    Add zip_code params

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get '/api/v1/zip_codes/:zip_code', to: 'zip_codes#show'
+
   namespace :api do
     namespace :v1 do
-      resources :zip_codes, only: [:index, :show]
+      get 'zip-codes/:zip_code', to: 'zip_codes#show'
     end
   end
 end
