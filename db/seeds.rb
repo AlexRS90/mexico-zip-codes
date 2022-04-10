27d3ae1ec4a5e0475f48a7b95ca4667ba069cb7b
AlexRS90 <aramosbengy@gmail.com>
2022-04-09 22:17:58 -0500

    Add records into the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+csv_codes = File.read(Rails.root.join('db/zip_codes.csv'))
+zip_codes = CSV.parse(csv_codes, headers: true, encoding: 'ISO-8859-1')
+
+zip_codes.each do |row|
+  record = Code.new
+  record.d_codigo = row['d_codigo']
+  record.d_asenta = row['d_asenta']
+  record.d_tipo_asenta = row['d_tipo_asenta']
+  record.d_mnpio = row['d_mnpio']
+  record.d_estado = row['d_estado']
+  record.d_ciudad = row['d_ciudad']
+  record.d_CP = row['d_CP']
+  record.c_estado = row['c_estado']
+  record.c_oficina = row['c_oficina']
+  record.c_CP = row['c_CP']
+  record.c_tipo_asenta = row['c_tipo_asenta']
+  record.c_mnpio = row['c_mnpio']
+  record.id_asenta_cpcons = row['id_asenta_cpcons']
+  record.d_zona = row['d_zona']
+  record.c_cve_ciudad = row['c_cve_ciudad']
+  record.save
+end
