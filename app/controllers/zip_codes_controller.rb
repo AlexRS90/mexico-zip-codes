d6e4e5c0ee734fc68c7e1c4ef77916cbc1832dbf
AlexRS90 <aramosbengy@gmail.com>
2022-04-09 22:29:55 -0500

    Add endpoints routes

---
+++ app/controllers/zip_codes_controller.rb
@@ -0,0 +1,7 @@
+class ZipCodesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
---
+++ test/controllers/zip_codes_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class ZipCodesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get zip_codes_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get zip_codes_show_url
+    assert_response :success
+  end
+end
