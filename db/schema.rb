c5aa497896567fc8d1264d471271fa0aaba45dbb
AlexRS90 <aramosbengy@gmail.com>
2022-04-10 01:18:59 -0500

    Refactor migration, schema and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -15,21 +15,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_10_002520) do
   enable_extension "plpgsql"
 
   create_table "codes", force: :cascade do |t|
-    t.integer "d_codigo"
-    t.string "d_asenta", limit: 200
-    t.string "d_tipo_asenta", limit: 200
-    t.string "d_mnpio", limit: 200
-    t.string "d_estado", limit: 200
-    t.string "d_ciudad", limit: 200
-    t.integer "d_CP"
-    t.integer "c_estado"
-    t.integer "c_oficina"
-    t.integer "c_CP"
-    t.integer "c_tipo_asenta"
-    t.integer "c_mnpio"
-    t.integer "id_asenta_cpcons"
-    t.string "d_zona", limit: 200
-    t.integer "c_cve_ciudad"
+    t.integer "zip_code"
+    t.string "locality", limit: 200
+    t.string "federal_entity", limit: 200
+    t.text "settlements"
+    t.string "municipality", limit: 200
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,14 @@ zip_codes = CSV.parse(csv_codes, headers: true, encoding: 'ISO-8859-1')
 
 zip_codes.each do |row|
   record = Code.new
-  record.d_codigo = row['d_codigo']
-  record.d_asenta = row['d_asenta']
-  record.d_tipo_asenta = row['d_tipo_asenta']
-  record.d_mnpio = row['d_mnpio']
-  record.d_estado = row['d_estado']
-  record.d_ciudad = row['d_ciudad']
-  record.d_CP = row['d_CP']
-  record.c_estado = row['c_estado']
-  record.c_oficina = row['c_oficina']
-  record.c_CP = row['c_CP']
-  record.c_tipo_asenta = row['c_tipo_asenta']
-  record.c_mnpio = row['c_mnpio']
-  record.id_asenta_cpcons = row['id_asenta_cpcons']
-  record.d_zona = row['d_zona']
-  record.c_cve_ciudad = row['c_cve_ciudad']
+  record.zip_code = row['d_codigo']
+  record.locality = row['d_ciudad']
+  record.federal_entity = row['d_estado']
+  record.settlements = [{
+    'name': row['d_asenta'],
+    'zone_type': row['d_zona'],
+    'settlement_type': row['d_tipo_asenta']
+  }]
+  record.municipality = row['d_mnpio']  
   record.save
 end
