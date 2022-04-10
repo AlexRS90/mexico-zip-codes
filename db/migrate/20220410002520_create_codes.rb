d4f7d83f8ce384a1763ef01045ee4b71f1c7a5f6
AlexRS90 <aramosbengy@gmail.com>
2022-04-09 22:16:55 -0500

    Refactor limit characters

--- db/migrate/20220410002520_create_codes.rb
+++ db/migrate/20220410002520_create_codes.rb
@@ -2,11 +2,11 @@ class CreateCodes < ActiveRecord::Migration[7.0]
   def change
     create_table :codes do |t|
       t.integer :d_codigo
-      t.string :d_asenta, limit: 50
-      t.string :d_tipo_asenta, limit: 50
-      t.string :d_mnpio, limit: 100
-      t.string :d_estado, limit: 50
-      t.string :d_ciudad, limit: 100
+      t.string :d_asenta, limit: 200
+      t.string :d_tipo_asenta, limit: 200
+      t.string :d_mnpio, limit: 200
+      t.string :d_estado, limit: 200
+      t.string :d_ciudad, limit: 200
       t.integer :d_CP
       t.integer :c_estado
       t.integer :c_oficina
@@ -14,7 +14,7 @@ class CreateCodes < ActiveRecord::Migration[7.0]
       t.integer :c_tipo_asenta
       t.integer :c_mnpio
       t.integer :id_asenta_cpcons
-      t.string :d_zona, limit: 50
+      t.string :d_zona, limit: 200
       t.integer :c_cve_ciudad
 
       t.timestamps
