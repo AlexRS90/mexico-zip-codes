963da6399a8057e9d1283024890243f923b11898
AlexRS90 <aramosbengy@gmail.com>
2022-04-09 22:20:06 -0500

    Fix linters

--- app/models/code.rb
+++ app/models/code.rb
@@ -6,6 +6,6 @@ class Code < ApplicationRecord
   validates :d_estado, presence: true
   validates :d_ciudad, presence: true
   validates :d_CP, presence: true
-  validates :c_estado, presence: true, numericality: { minimum: 1, maximum: 32}
+  validates :c_estado, presence: true, numericality: { minimum: 1, maximum: 32 }
   validates :c_oficina, presence: true
 end
